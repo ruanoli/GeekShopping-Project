36b58cfa250209aabb50a550e8d3955781b01762
Ruan Oliveira <ruanoliveira.pro@gmail.com>
2023-05-25 21:41:08 -0300

    Create Models and Migrations

---
+++ GeekShopping.API/Model/Base/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GeekShopping.API.Model.Base
+{
+    public class BaseEntity
+    {
+        [Key]
+        public long Id { get; set; }
+    }
+}
