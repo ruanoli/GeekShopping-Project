b827d4516544c99c7a0ef92b0cd6a7196ce2273e
Ruan Oliveira <ruanoliveira.pro@gmail.com>
2023-06-11 11:35:08 -0300

    front product

---
+++ GeekShopping.Web/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using GeekShopping.Web.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productService.GetAllProducts();
+            return View(products);
+        }
+    }
+}
